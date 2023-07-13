e9a1ffb795f924e0a567ab99ad9d2784a7e3e966
chechyotka <romanchechyotkin@gmail.com>
2023-07-14 00:19:09 +0300

    init

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/romanchechyotkin/prettylogger
+
+go 1.20
+
+require (
+	github.com/fatih/color v1.15.0
+	golang.org/x/exp v0.0.0-20230713183714-613f0c0eb8a1
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"log"
+
+	"github.com/fatih/color"
+	"golang.org/x/exp/slog"
+)
+
+type PrettyHandlerOptions struct {
+	SlogOpts slog.HandlerOptions
+}
+
+type PrettyHandler struct {
+	slog.Handler
+	l *log.Logger
+}
+
+func NewPrettyHandler(out io.Writer, opts PrettyHandlerOptions) *PrettyHandler {
+	h := &PrettyHandler{
+		Handler: slog.NewTextHandler(out, &opts.SlogOpts),
+		l:       log.New(out, "", 0),
+	}
+	return h
+}
+
+func (h *PrettyHandler) Handle(ctx context.Context, r slog.Record) error {
+	level := r.Level.String() + ":"
+
+	switch r.Level {
+	case slog.LevelDebug:
+		level = color.MagentaString(level)
+	case slog.LevelInfo:
+		level = color.BlueString(level)
+	case slog.LevelWarn:
+		level = color.YellowString(level)
+	case slog.LevelError:
+		level = color.RedString(level)
+	}
+
+	fields := make(map[string]interface{}, r.NumAttrs())
+	r.Attrs(func(attr slog.Attr) bool {
+		fields[attr.Key] = attr.Value.Any()
+		return true
+	})
+
+	b, err := json.MarshalIndent(fields, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	timeStr := r.Time.Format("[15:05:05.000]")
+	msg := color.CyanString(r.Message)
+
+	h.l.Println(timeStr, level, msg, string(b))
+
+	return nil
+}
